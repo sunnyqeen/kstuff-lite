0b863c0de2267ef5c9400461065d634b873ce06d
SunnyQeen <sunnyqeen@gmail.com>
2026-02-16 19:44:06 +0100

    mbedtls

--- ps5-kstuff/build_libtomcrypt.sh
+++
@@ -1,2 +0,0 @@
-cd libtomcrypt
-for i in clean -j$(nproc); do make CC='gcc -nostdlib -nostdinc -isystem /proc/'$$'/cwd/../../freebsd-headers  -isystem '"'${x86intrin_path//"'"/"'"'"'"'"'"'"'"}'"' -O3 -march=znver2 -g -ffreestanding -ffunction-sections -fdata-sections -fPIE -fPIC -fvisibility=hidden -include /proc/'$$'/cwd/../overrides.h' $i; done
