015796386a9721e5c77752fd1fb8fbd6f62a43f6
Roman Tarasov <i@drakmor.ru>
2026-03-16 19:42:41 +0300

    uelf: make FPU entry failures explicit

--- ps5-kstuff/uelf/fpu.c
+++ ps5-kstuff/uelf/fpu.c
@@ -3,20 +3,37 @@
 __attribute__((aligned(64))) static char xsave_area[4096]; //is this enough?
 static uint32_t xsave_eax, xsave_edx;
 static uint64_t saved_cr0;
+static uint32_t fpu_depth;
+static uint32_t fpu_state_saved;
 
-void uelf_fpu_enter(void)
+int uelf_fpu_enter(void)
 {
-    saved_cr0 = read_cr0();
-    write_cr0(saved_cr0 & -9); //clear CR0.TS
+    if(fpu_depth++)
+        return 0;
+    fpu_state_saved = 0;
+    if(read_cr0_checked(&saved_cr0) || write_cr0_checked(saved_cr0 & -9)) //clear CR0.TS
+    {
+        fpu_depth = 0;
+        return 1;
+    }
     asm volatile("xgetbv":"=d"(xsave_edx),"=a"(xsave_eax):"c"(0));
     asm volatile("xsave %0":"=m"(xsave_area):"a"(xsave_eax),"d"(xsave_edx));
     asm volatile("finit");
     uint32_t mxcsr = 0x1f80;
     asm volatile("ldmxcsr %0"::"m"(mxcsr));
+    fpu_state_saved = 1;
+    return 0;
 }
 
 void uelf_fpu_exit(void)
 {
+    if(!fpu_depth)
+        return;
+    if(--fpu_depth)
+        return;
+    if(!fpu_state_saved)
+        return;
     asm volatile("xrstor %0"::"m"(xsave_area),"a"(xsave_eax),"d"(xsave_edx));
-    write_cr0(saved_cr0);
+    write_cr0_checked(saved_cr0);
+    fpu_state_saved = 0;
 }
