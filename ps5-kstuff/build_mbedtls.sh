91a67a08c4ebda782d2a11b8052b52d3b4333345
SunnyQeen <sunnyqeen@gmail.com>
2026-03-20 23:18:27 +0100

    mbedtls shani

--- ps5-kstuff/build_mbedtls.sh
+++ ps5-kstuff/build_mbedtls.sh
@@ -1,4 +1,5 @@
 cd mbedtls
 #sed -i '/#define MBEDTLS_AESNI_C/d' include/mbedtls/mbedtls_config.h
+git apply ../sha-ni.patch
 x86intrin_path=$(dirname "$(find /usr/lib/gcc -name "x86intrin.h" -print -quit 2>/dev/null)")
 for i in clean "lib -j$(nproc)"; do make CC='gcc -nostdlib -nostdinc -isystem /proc/'$$'/cwd/../../freebsd-headers -isystem '"'${x86intrin_path//"'"/"'"'"'"'"'"'"'"}'"' -O3 -march=znver2 -g -ffreestanding -ffunction-sections -fdata-sections -fvisibility=hidden' $i; done
