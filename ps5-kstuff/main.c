3ea4f2a514a22e0c2cc05086e6ead72a1435afd1
SunnyQeen <sunnyqeen@gmail.com>
2026-06-08 22:15:36 +0200

    Update version to 1.07

--- ps5-kstuff/main.c
+++ ps5-kstuff/main.c
@@ -975,7 +975,7 @@ int main(void* ds, int a, int b, uintptr_t c, uintptr_t d)
 
     // Header
     const char *hdr =
-        "Welcome To Kstuff Lite 1.06\nPlayStation 5 FW: ";
+        "Welcome To Kstuff Lite 1.07 UFS/EXFAT\nPlayStation 5 FW: ";
     while (*hdr) *p++ = *hdr++;
 
     // Major
